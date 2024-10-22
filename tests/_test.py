4a07bbb42e5a4883e5f614323aea623cbe94d8db
vitayushchyk <vitay2715@gmail.com>
2024-10-22 12:32:09 +0300

    - added minimal project settings, - actions github action, - upd README.md

---
+++ tests/_test.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_health_check():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"status_code": 200, "detail": "ok", "result": "working"}
