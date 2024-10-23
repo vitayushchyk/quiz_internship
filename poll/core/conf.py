ee868d30e5a926c15976d702fcdff5c0a08679be
vitayushchyk <vitay2715@gmail.com>
2024-10-23 19:11:35 +0300

    add Dockerfile

---
+++ poll/core/conf.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    cors_origins: list[str] = ["*"]
+    cors_allow_credentials: bool = True
+    cors_allow_methods: list[str] = ["*"]
+    cors_allow_headers: list[str] = ["*"]
+
+
+settings = Settings()
