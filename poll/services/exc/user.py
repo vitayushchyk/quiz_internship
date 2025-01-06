5175d7443a4e503aa2bfe99d78271d836cca868f
Vita Yushchyk <151214591+vitayushchyk@users.noreply.github.com>
2025-01-06 14:24:17 +0200

    Be 7 add validation (#7)
    * deleting your own account has been implemented
    
    * Restrict user update and delete actions to authorized users
    
    Added checks to ensure that users can only update or delete their own profiles.
    
    * upd ci/cd
    
    ---------
    
    Co-authored-by: vitayushchyk <vitay2715@gmail.com>

--- poll/services/exc/user.py
+++ poll/services/exc/user.py
@@ -9,7 +9,10 @@ class UserAlreadyExist(Exception):
 
 
 class UserNotAuthenticated(Exception):
-    def __init__(
-        self,
-    ):
+    def __init__(self):
         self.detail = "Incorrect email or password"
+
+
+class UserForbidden(Exception):
+    def __init__(self):
+        self.detail = "Access to the requested resource is forbidden."
